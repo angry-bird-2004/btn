247062c11b1eb8ee68c4248fdfd57f4444abb1e1
saif <saifibr2004@gmail.com>
2026-07-15 17:15:07 +0500

    .

--- backend/api/index.js
+++ backend/api/index.js
@@ -13,7 +13,7 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 
-app.get('/api/message', (req, res) => {
+app.get('./', (req, res) => {
     res.json({ text: "Hello from the Express server!" });
 });
 
