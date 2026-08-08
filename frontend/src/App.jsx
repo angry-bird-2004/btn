e78c0634108dbdd5e8e5a2c8a4d49367a48698fe
saif <saifibr2004@gmail.com>
2026-08-08 13:47:20 +0500

    in

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -31,7 +31,7 @@ console.log("baseUrl:", baseUrl);
 
   return (
     <div>
-      <button onClick={handleClick}>Get Message vvv</button>
+      <button onClick={handleClick}>Get Message in testing</button>
       <p>{message}</p>
     </div>
   );
